86f8b17b5f75ff3f70eb2a2ebcf8e4448a564409
harshpandey002 <coding.harshp@gmail.com>
2024-10-08 23:45:19 +0530

    Wallet Connectors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { makeServer } from "./server";
-import { BrowserRouter as Router } from "react-router-dom";
-import { DataProvider } from "./contexts/DataProvider.js";
-import { AuthProvider } from "./contexts/AuthProvider.js";
-import { UserProvider } from "./contexts/UserDataProvider.js";
-import { AddressProvider } from "./contexts/AddressProvider.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { makeServer } from './server';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { DataProvider } from './contexts/DataProvider.js';
+import { AuthProvider } from './contexts/AuthProvider.js';
+import { UserProvider } from './contexts/UserDataProvider.js';
+import { AddressProvider } from './contexts/AddressProvider.js';
+import { WagmiProvider } from 'wagmi';
+import { wagmiConfig } from './helpers/wagmiConfig.js';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // Call make Server
 makeServer();
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,12 +24,16 @@ ReactDOM.render(
         <DataProvider>
           <UserProvider>
             <AddressProvider>
-              <App />
+              <WagmiProvider config={wagmiConfig}>
+                <QueryClientProvider client={queryClient}>
+                  <App />
+                </QueryClientProvider>
+              </WagmiProvider>
             </AddressProvider>
           </UserProvider>
         </DataProvider>
       </AuthProvider>
     </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
